02296939804a10c4f6c74c36d823e9876e0ee13a
shubham <srkale856@gmail.com>
2024-05-18 18:21:37 +0530

    this is the develop changes done by the person X

---
+++ src/test/java/stepdefination/SeleniumTest.java
@@ -0,0 +1,17 @@
+package stepdefination;
+
+public class SeleniumTest 
+{
+	public void Test3()
+	{
+		System.out.println("this is the new test added by person X to the Develop changes");
+	}
+	
+	public void Test$()
+	{
+		System.out.println("this is the new test added by person X to the develop changes");
+	}
+	
+	
+
+}
